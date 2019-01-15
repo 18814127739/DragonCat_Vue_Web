3152ac7bf971bb820354702f01b991de4d9debfd
tangjianming <tang.jianming@ztesoft.com>
2019-01-15 10:24:44 +0800

    update:个人主页css调整

--- src/pages/Personal.vue
+++ src/pages/Personal.vue
@@ -1,24 +1,26 @@
 <template>
   <PageContainer>
     <div class="personal-page">
-      <div class="skills">
-        <p>专业技能</p>
-        <ul>
-          <li
-            v-for="item in skills"
-            :key="item._id"
-          >
-            <div class="name">
-              {{item.name}}
-            </div>
-            <div class="line">
-              <div
-                class="cover"
-                :style="{width:`${item.degree}%`}"
-              ></div>
-            </div>
-          </li>
-        </ul>
+      <div class="left">
+        <div class="skills">
+          <p>专业技能</p>
+          <ul>
+            <li
+              v-for="item in data.skills"
+              :key="item._id"
+            >
+              <div class="name">
+                {{item.name}}
+              </div>
+              <div class="line">
+                <div
+                  class="cover"
+                  :style="{width:`${item.degree}%`}"
+                ></div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
       <div class="right">
         <div class="introduction">
@@ -34,17 +36,20 @@ import api from "@services";
 export default {
   data() {
     return {
+      data: {
+        skills: []
+      },
       skills: [
-        { _id: 0, name: "javascript", degree: 82 },
-        { _id: 1, name: "http", degree: 76 },
-        { _id: 2, name: "es6", degree: 73 },
-        { _id: 3, name: "css", degree: 73 },
-        { _id: 4, name: "react", degree: 73 },
-        { _id: 5, name: "vue", degree: 68 },
-        { _id: 6, name: "node.js", degree: 68 },
-        { _id: 7, name: "mongodb", degree: 68 },
-        { _id: 8, name: "webpack", degree: 68 },
-        { _id: 9, name: "java", degree: 58 }
+        { name: "javascript", degree: 83 },
+        { name: "http", degree: 77 },
+        { name: "es6", degree: 74 },
+        { name: "css", degree: 74 },
+        { name: "react", degree: 74 },
+        { name: "vue", degree: 69 },
+        { name: "node.js", degree: 69 },
+        { name: "mongodb", degree: 69 },
+        { name: "webpack", degree: 69 },
+        { name: "java", degree: 58 }
       ]
     };
   },
@@ -57,7 +62,7 @@ export default {
         userId: this.$store.state.userInfo._id
       };
       const res = await api.getHomePageInfo(params);
-      console.log(res);
+      this.data = res;
     }
   }
 };
@@ -68,37 +73,46 @@ export default {
   padding: 16px 32px 46px;
   display: flex;
   color: #3e3e3e;
-  .skills {
-    p {
-      margin-top: 0;
-      font-size: 16px;
-      font-weight: bold;
-    }
-    width: 280px;
-    border: 1px solid #3e3e3e;
-    border-radius: 12px;
-    padding: 12px 20px 20px;
-    ul {
-      li {
-        margin-top: 12px;
-        .name {
-          color: #3e3e3e;
-          margin-left: 2px;
-        }
-        .line {
-          margin-top: 4px;
-          height: 14px;
-          background: rgba(94, 94, 94, 0.8);
-          .cover {
-            height: 100%;
-            width: 60%;
-            background: -webkit-gradient(
-              linear,
-              left 0,
-              right 0,
-              from(#fff0f5),
-              to(#bfefff)
-            );
+  .left {
+    padding: 8px;
+    border-radius: 15px;
+    // background: #bfefff;
+    background: rgba(191, 239, 255, 0.9);
+    .skills {
+      z-index: 1;
+      background: white;
+      p {
+        margin-top: 0;
+        font-size: 16px;
+        font-weight: bold;
+      }
+      width: 280px;
+      // border: 1px solid #3e3e3e;
+      border-radius: 12px;
+      padding: 12px 20px 20px;
+      ul {
+        li {
+          margin-top: 12px;
+          .name {
+            color: #3e3e3e;
+            margin-left: 2px;
+          }
+          .line {
+            margin-top: 4px;
+            height: 14px;
+            background: rgba(94, 94, 94, 0.8);
+            .cover {
+              height: 100%;
+              width: 60%;
+              background: -webkit-gradient(
+                linear,
+                left 0,
+                right 0,
+                from(#eed2ee),
+                to(#ee82ee)
+              );
+              // background: #bfefff;s
+            }
           }
         }
       }
