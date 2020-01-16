9113fda4c07b765ef5bb736db335925a016df488
tangjianming <tang.jianming@iwhalecloud.com>
2020-01-16 22:32:31 +0800

    update: 登录页css

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,122 +1,58 @@
 <template>
   <div class="login">
-    <div class="left-pane">
-      <div class="content">
-        <img />
+    <div class="wrap space-around">
+      <div class="left-pane">
+        <div class="content">
+          <img />
+        </div>
       </div>
-    </div>
-    <div class="right-pane">
-      <el-tabs
-        v-model="activeTab"
-        type="border-card"
-      >
-        <el-tab-pane
-          label="登录"
-          name="login"
-        >
-          <el-form
-            :model="loginInfo"
-            :rules="loginRules"
-            label-position="top"
-          >
-            <el-form-item
-              label="账号："
-              prop="account"
-            >
-              <el-input
-                size="medium"
-                placeholder="用户名/手机号/邮箱"
-                v-model="loginInfo.account"
-              ></el-input>
-            </el-form-item>
-            <el-form-item
-              label="密码："
-              prop="password"
-            >
-              <el-input
-                type="password"
-                v-model="loginInfo.password"
-                autocomplete="off"
-                size="medium"
-              ></el-input>
-            </el-form-item>
-            <el-form-item
-              class="btn-item"
-              v-loading="loginLoading"
-            >
-              <el-button
-                type="primary"
-                size="medium"
-                @click="login"
-              >登录</el-button>
-            </el-form-item>
-          </el-form>
-        </el-tab-pane>
-        <el-tab-pane
-          label="注册"
-          name="register"
-        >
-          <el-form
-            :model="registerInfo"
-            :rules="registerRules"
-            label-position="top"
-          >
-            <el-form-item
-              label="用户名："
-              prop="userName"
-            >
-              <el-input
-                v-model="registerInfo.userName"
-                maxlength="20"
-                size="medium"
-              ></el-input>
-            </el-form-item>
-            <el-form-item
-              label="手机号："
-              prop="phone"
-            >
-              <el-input
-                v-model="registerInfo.phone"
-                maxlength="11"
-                size="medium"
-              ></el-input>
-            </el-form-item>
-            <el-form-item
-              label="邮箱："
-              prop="eMail"
-            >
-              <el-input
-                type="eMail"
-                v-model="registerInfo.eMail"
-                maxlength="40"
-                size="medium"
-              ></el-input>
-            </el-form-item>
-            <el-form-item
-              label="密码："
-              prop="password"
-            >
-              <el-input
-                type="password"
-                v-model="registerInfo.password"
-                autocomplete="off"
-                maxlength="20"
-                size="medium"
-              ></el-input>
-            </el-form-item>
-            <el-form-item
-              class="btn-item"
-              v-loading="registerLoading"
-            >
-              <el-button
-                type="primary"
-                size="medium"
-                @click="register"
-              >注册</el-button>
-            </el-form-item>
-          </el-form>
-        </el-tab-pane>
-      </el-tabs>
+      <div class="right-pane">
+        <el-tabs v-model="activeTab" type="border-card">
+          <el-tab-pane label="登录" name="login">
+            <el-form :model="loginInfo" :rules="loginRules" label-position="top">
+              <el-form-item label="账号：" prop="account">
+                <el-input size="medium" placeholder="用户名/手机号/邮箱" v-model="loginInfo.account"></el-input>
+              </el-form-item>
+              <el-form-item label="密码：" prop="password">
+                <el-input
+                  type="password"
+                  v-model="loginInfo.password"
+                  autocomplete="off"
+                  size="medium"
+                ></el-input>
+              </el-form-item>
+              <el-form-item class="btn-item" v-loading="loginLoading">
+                <el-button type="primary" size="medium" @click="login">登录</el-button>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+          <el-tab-pane label="注册" name="register">
+            <el-form :model="registerInfo" :rules="registerRules" label-position="top">
+              <el-form-item label="用户名：" prop="userName">
+                <el-input v-model="registerInfo.userName" maxlength="20" size="medium"></el-input>
+              </el-form-item>
+              <el-form-item label="手机号：" prop="phone">
+                <el-input v-model="registerInfo.phone" maxlength="11" size="medium"></el-input>
+              </el-form-item>
+              <el-form-item label="邮箱：" prop="eMail">
+                <el-input type="eMail" v-model="registerInfo.eMail" maxlength="40" size="medium"></el-input>
+              </el-form-item>
+              <el-form-item label="密码：" prop="password">
+                <el-input
+                  type="password"
+                  v-model="registerInfo.password"
+                  autocomplete="off"
+                  maxlength="20"
+                  size="medium"
+                ></el-input>
+              </el-form-item>
+              <el-form-item class="btn-item" v-loading="registerLoading">
+                <el-button type="primary" size="medium" @click="register">注册</el-button>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
     </div>
   </div>
 </template>
@@ -201,7 +137,11 @@ export default {
     },
     isValid(params) {
       let flag = true;
-      if (!/^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/.test(params.eMail)) {
+      if (
+        !/^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/.test(
+          params.eMail
+        )
+      ) {
         this.$message({
           type: "warning",
           message: "邮箱地址不符合格式要求"
@@ -222,7 +162,7 @@ export default {
 </script>
 <style lang="less">
 .login {
-  height: 100vh;
+  // height: 100vh;
   display: flex;
   align-items: center;
   background: -webkit-gradient(
@@ -232,6 +172,13 @@ export default {
     from(#fffafa),
     to(#f0ffff)
   );
+
+  .wrap {
+    margin: 0 auto;
+    height: 100vh;
+    width: 1200px;
+  }
+
   .left-pane {
     display: flex;
     flex-direction: column;
@@ -239,8 +186,7 @@ export default {
     justify-content: center;
     width: 480px;
     height: 480px;
-    position: fixed;
-    left: 10vw;
+    position: relative;
     .content {
       width: 300px;
       height: 300px;
@@ -280,10 +226,7 @@ export default {
     animation: backRotateMove infinite linear 8s;
   }
   .right-pane {
-    position: fixed;
     width: 400px;
-    top: 20vh;
-    right: 10vw;
     .el-form {
       width: 100%;
       .el-form-item {
